defb9a073dc2a43d97bf5b7ba4dffd67d89b2f4c
portuga <joaopedromateus6@gmail.com>
2022-05-18 15:36:51 -0300

    Requisito 1

---
+++ src/components/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class NotFound extends React.Component {
+  render() {
+    console.log('notFound');
+    return (
+      <div data-testid="page-not-found" />
+    );
+  }
+}
+
+export default NotFound;
